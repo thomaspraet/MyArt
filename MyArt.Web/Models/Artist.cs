60a3cd8a11c48822bbe91952c4a5437a82c7c554
thomaspraet <38831042+thomaspraet@users.noreply.github.com>
2026-09-05 13:52:37 +0200

    Add project files.

---
+++ MyArt.Web/Models/Artist.cs
@@ -0,0 +1,12 @@
+﻿namespace MyArt.Web.Models
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public string PlaceOfBirth { get; set; } = string.Empty;
+        public string YearOfBirth { get; set; } = string.Empty;
+    }
+}
